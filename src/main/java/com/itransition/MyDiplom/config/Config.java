ceb25ab1e8378cde7e55a3b4a7b456e56194d03a
VladislavGovaka <87976951+VladislavGovaka@users.noreply.github.com>
2021-08-26 22:18:53 +0300

    Add files via upload

---
+++ src/main/java/com/itransition/MyDiplom/config/Config.java
@@ -0,0 +1,13 @@
+package com.itransition.MyDiplom.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Config implements WebMvcConfigurer {
+
+
+
+}
